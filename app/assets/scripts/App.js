3ce95c305adbb93af27a3ddb26123e09bb65b27e
Rutger <kroon.rj@gmail.com>
2020-06-13 19:56:50 +0200

    Completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,11 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
-import RevealOnScroll from './modules/RevealOnScroll'
-
+// import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 // let revealOnScroll = new RevealOnScroll();
 
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 
 if(module.hot) {
   module.hot.accept()
